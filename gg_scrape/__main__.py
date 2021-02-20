7d64b2719cd674b05a87575812acf7317854067f
teauxfu <teauxfu@icloud.com>
2021-02-20 16:51:48 -0600

    Update __main__.py

--- gg_scrape/__main__.py
+++ gg_scrape/__main__.py
@@ -1,4 +1,4 @@
-# this file exists to support calling the package as 'python -m pip ggs'
+# this file exists to support calling the package as 'python -m ggs'
 # https://typer.tiangolo.com/tutorial/package/#support-python-m-optional
 
 from gg_scrape.argsparse import ggs
