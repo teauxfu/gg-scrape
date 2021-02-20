df563d54474e760acd097da82022e96f5c2a7f0a
teauxfu <alexmw777@gmail.com>
2021-02-20 12:31:48 -0600

    remove the vanilla scraper

--- gg_scrape/argsparse.py
+++ gg_scrape/argsparse.py
@@ -4,7 +4,6 @@ import typer
 
 from .champion_gg_scraper import champion_gg_scraper
 from .mobalytics_scraper import mobalytics_scraper
-# from .vanilla_scraper import vanilla_scraper
 from .print_tree import print_tree
 
 ggs = typer.Typer()
@@ -15,7 +14,7 @@ def main(
     champion: str = typer.Argument(..., help="The champion you're playing"),
     role: str = typer.Argument("", help="The role you're playing", show_default=False),
     matchup: str = typer.Argument("", help="Your opposing matchup", show_default=False),
-    scraper: str = typer.Argument("m", help="Which site to scrape", show_default=False),
+    scraper: str = typer.Argument("mobalytics", help="Which site to scrape", show_default=True),
     verbose: str = typer.Option(False, help="Show more verbose output"),
 ):
     args = champion, role, matchup, verbose
@@ -32,8 +31,6 @@ def main(
     elif role.startswith("s"):
         role = "support"
 
-#    if scraper.startswith("v"):
-#        print_tree(vanilla_scraper(*args))
     if scraper.startswith("m"):
         print_tree(mobalytics_scraper(*args))
     elif scraper.startswith("c"):
