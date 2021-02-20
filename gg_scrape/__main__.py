b82869b009b735b983b7c7da71c1a03297143104
teauxfu <alexmw777@gmail.com>
2021-02-20 00:29:56 -0600

    route script entry point to argsparse

--- gg_scrape/__main__.py
+++ gg_scrape/__main__.py
@@ -1,5 +1,5 @@
 # this file exists to support calling the package as 'python -m pip ggs'
 # https://typer.tiangolo.com/tutorial/package/#support-python-m-optional
 
-from gg_scrape.main import ggs
+from gg_scrape.argsparse import ggs
 ggs(prog_name='ggs')
